5e6694a7d19b78c3c50848d5dfe6a352edbe2d0a
jdeklijn <57561513+jdeklijn@users.noreply.github.com>
2024-11-12 13:09:15 +0100

    after video 1
    Created and linked index, mainview, styling and bootstrap

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
-<!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+
+/* The plan:
+ * ==== Fase 1, create a github for the viewers
+ * Fase 2, a simple userinterface (html/css/js)
+ * Fase 3, basic php forms (security)
+ * Fase 4, Basic MySQL database and connecting to php
+ * Fase 5, advanced PHP functions (include) error handling (try/catch)
+ * Fase 5, advanced userinterface multilanguage GUI
+ * Fase 6, advanced MySQL
+ * Fase 7, PHP OOP, classes (require)
+ * Fase 8, Model/view/controller MVC
+ * Fase 9, Usermanagement roles (GUI/MySQL)
+ * Fase 10, expert interface with JS and free/payed content
+ * Fase 11, (pen)testing offline
+ * Fase 12, bugfixing
+ * Fase 13, the rest, what ever is left
+ */
+
+header('location:mainview.php');
---
+++ mainview.php
@@ -0,0 +1,12 @@
+<?php
+echo('<!doctype=html><html><head>');
+echo('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">');
+echo('<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>');
+echo('<link rel="stylesheet" href="./styles/mainstyle.css">');
+echo('</head><body>');
+
+echo('<div class="h3">The bootstrap styling is active</div>');
+
+echo('</body></html>');
+
+
